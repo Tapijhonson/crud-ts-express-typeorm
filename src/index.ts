4c7927bef96574c5f4bfaadb5ca87d543679b9c9
Tapijhonson <moisesporto@hotmail.com>
2024-08-16 23:39:40 -0300

    Configuração do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import "reflect-metadata"
+import { AppDataSource } from "./database/data-source";
+
+
+const main = async () => {
+    try {
+        await AppDataSource.initialize();
+        console.log("Banco de dados conectado com sucesso!");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+main();
